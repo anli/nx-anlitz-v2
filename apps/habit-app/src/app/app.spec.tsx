a0d01568fc6d363fe0e6fe1658f6cd39ee34760a
Su Anli <su.anli@anlitz.com>
2021-10-17 10:14:37 +0800

    chore(habit-app): setup integrated test with coverage

--- apps/habit-app/src/app/app.spec.tsx
+++ apps/habit-app/src/app/app.spec.tsx
@@ -1,10 +1,9 @@
-import 'react-native';
-import React from 'react';
 import { render } from '@testing-library/react-native';
-
+import React from 'react';
+import 'react-native';
 import App from './App';
 
 it('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome')).toBeDefined();
 });
