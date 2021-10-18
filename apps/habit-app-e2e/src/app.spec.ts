542a88b1638cf61a94ce2f7ea0cec5916d216690
Su Anli <su.anli@anlitz.com>
2021-10-18 10:37:05 +0800

    chore(habit-app): add detox android on ci

--- apps/habit-app-e2e/src/app.spec.ts
+++ apps/habit-app-e2e/src/app.spec.ts
@@ -1,16 +1,11 @@
-import { device, element, by, expect } from 'detox';
+import { by, device, element, expect } from 'detox';
 
-describe('HabitApp', () => {
+describe('Habit App', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
   it('should display welcome message', async () => {
-    await expect(element(by.id('heading'))).toHaveText('Welcome to HabitApp');
-  });
-
-  it('should open nx link', async () => {
-    await expect(element(by.id('nx-link'))).toBeVisible();
-    element(by.id('nx-link')).tap();
+    await expect(element(by.text('Welcome'))).toBeVisible();
   });
 });
