15ae7cae541d658d843781131e2b57bdc0143f49
Su Anli <anli@users.noreply.github.com>
2021-10-23 17:30:04 +0800

    chore(habit-app): add react-navigation, home-screen (#7)

---
+++ apps/habit-app/jest.setup.ts
@@ -0,0 +1,8 @@
+// ReferenceError: You are trying to `import` a file after the
+// Jest environment has been torn down.
+// ../../node_modules/@react-navigation/native
+// /lib/commonjs/useLinking.native.tsx:
+jest.mock('@react-navigation/native/lib/commonjs/useLinking.native', () => ({
+  default: () => ({ getInitialState: { then: jest.fn() } }),
+  __esModule: true,
+}));
